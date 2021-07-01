703c024a217a32bdb12acc7e5e8ac99052ebf10a
Ruslan <rus_04@mail.ru>
2021-07-01 13:23:49 +0300

    sprint 4.03

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close')
 const profileName = document.querySelector('.profile__title')
 const profileProfession = document.querySelector('.profile__subtitle')
 const formElement = document.querySelector('.popup__form')
-const inputName = formElement.querySelector('.popup__input_name')
-const inputProfession = formElement.querySelector('.popup__input_profession')
+const inputName = formElement.querySelector('.popup__input_field_name')
+const inputProfession = formElement.querySelector('.popup__input_field_profession')
 
 
 function editButton() {
